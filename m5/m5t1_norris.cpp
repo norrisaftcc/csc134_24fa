9054bb22e153ba8485e9c60c59f38f8e40c4d34b
teacherbot <norrisa@faytechcc.edu>
2024-10-28 14:28:31 +0000

    m5t1 start

---
+++ m5/m5t1_norris.cpp
@@ -0,0 +1,16 @@
+// CSC 134
+// M5T1 - Basic Functions
+// norris
+// 10/28/24
+
+#include <iostream>
+using namespace std;
+
+// function prototypes (short version)
+
+// main
+int main() {
+    return 0;
+}
+
+// function definitions (full version)
