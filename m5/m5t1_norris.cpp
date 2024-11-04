a1435770c0fa89ccacd2e93cc5feec86c6b2d8b8
teacherbot <norrisa@faytechcc.edu>
2024-11-04 15:17:15 +0000

    previous assignment

--- m5/m5t1_norris.cpp
+++ m5/m5t1_norris.cpp
@@ -7,10 +7,31 @@
 using namespace std;
 
 // function prototypes (short version)
+void say_hello();
+int give_the_answer();
+int double_an_int(int number);
 
 // main
 int main() {
+    cout << "Hello from main" << endl;
+    say_hello();
+    cout << "The magic number is: ";
+    int number = give_the_answer();
+    cout << number << endl;
+    int twotimes = double_an_int(number);
+    cout << "Double that is " << twotimes << endl;
     return 0;
 }
 
 // function definitions (full version)
+void say_hello() {
+    cout << "Hello from say_hello()" << endl;
+}
+int give_the_answer() {
+    int answer = 42;
+    return answer;
+}
+int double_an_int(int number) {
+    int twotimes = number * 2;
+    return twotimes;
+}
